fcb58efd68ff2a49b00e6b6ebc0de418be11f7a2
mrice <mricekanal@gmail.com>
2024-04-29 23:14:28 +0300

    29.04.24 (23:14)

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import os
 import shutil
 
+path: str = input("Please input absolute path to your folder: ")
 
-path_to_folder: str = os.path.join(input("Enter the absolute path to your folder: "))
-EXTENSIONS: dict = {
-    "image": ["png", "jpg", "jpeg"],
-    "video": ["mp4", "avi", "mkv"],
-    "audio": ["mp3"],
-    "gif": ["gif"],
-    "icon": ["ico"],
-}
+try:
+    list_files = os.listdir(path=path)
+    list_files.remove('desktop.ini')
 
+    print(f"Folder located in '{path}' contains:")
+    for i in range(len(list_files)):
+        print(f"{i+1}. {list_files[i]}")
 
-def sorting_function(directory: str) -> None:
-    """
-    :param directory:
-    :return:
-    """
-    try:
-        folder = os.listdir(path=directory)
-        exists_folders = [fold for fold in folder if len(fold.split(".")) < 2 and fold in EXTENSIONS.keys()]
-        for extension in EXTENSIONS:
-            os.mkdir(path=os.path.join(directory, extension)) if extension not in exists_folders else None
-        for file in folder:
-            if len(file.split(".")) >= 2:
-                for name, ex in EXTENSIONS.items():
-                    if file.split(".")[-1] in ex:
-                        shutil.move(os.path.join(directory, file), os.path.join(directory, name, file))
-    except NotADirectoryError as e:
-        print(f"Sorry, the file could not be found.\n{e}")
-    finally:
-        for dirname in os.listdir(path=directory):
-            if len(os.listdir(path=os.path.join(directory, dirname))) == 0:
-                os.rmdir(path=os.path.join(directory, dirname))
-        print("Done")
+    answer: str = input("Are you sure? (y/n): ")
 
-
-def main():
-    sorting_function(directory=path_to_folder)
-
-
-if __name__ == "__main__":
-    main()
+    match answer:
+        case "Y" | "y":
+            try:
+                extensions_list = []
+                for file in list_files:
+                    if len(file.split(".")) >= 2:
+                        if file.split(".")[-1] not in extensions_list:
+                            extensions_list.append(file.split(".")[-1])
+                for ext in extensions_list:
+                    try:
+                        os.mkdir(path=os.path.join(path, ext))
+                    except FileExistsError:
+                        pass
+                for file in list_files:
+                    if len(file.split(".")) >= 2:
+                        ext = file.split(".")[-1]
+                        shutil.move(os.path.join(path, file), os.path.join(path, ext, file))
+            except Exception as e:
+                print(f"Something went wrong. {e}")
+            else:
+                print("All done")
+        case "N" | "n":
+            print("Okay")
+        case _:
+            print("Incorrect input")
+except FileNotFoundError:
+    print("Oops! Something went wrong.")
+    print("Please try again.")
